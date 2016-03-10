378560b6db9a3da24d039710f1c743f760c39ea7
Eric McCormick <edm00se@gmail.com>
2016-03-10 15:34:58 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# generator-xsp [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> a yeoman generator to scaffold an XPages runtime (xsp) compliant On Disk Project (ODP)
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-xsp using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-xsp
+```
+
+Then generate your new project:
+
+```bash
+yo xsp
+```
+
+## Getting To Know Yeoman
+
+Yeoman has a heart of gold. He&#39;s a person with feelings and opinions, but he&#39;s very easy to work with. If you think he&#39;s too opinionated, he can be easily convinced. Feel free to [learn more about him](http://yeoman.io/).
+
+## License
+
+MIT © [Eric McCormick](https://edm00se.io/)
+
+
+[npm-image]: https://badge.fury.io/js/generator-xsp.svg
+[npm-url]: https://npmjs.org/package/generator-xsp
+[travis-image]: https://travis-ci.org/edm00se/generator-xsp.svg?branch=master
+[travis-url]: https://travis-ci.org/edm00se/generator-xsp
+[daviddm-image]: https://david-dm.org/edm00se/generator-xsp.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/edm00se/generator-xsp
+[coveralls-image]: https://coveralls.io/repos/edm00se/generator-xsp/badge.svg
+[coveralls-url]: https://coveralls.io/r/edm00se/generator-xsp
---
+++ generators/app/index.js
@@ -0,0 +1,85 @@
+'use strict';
+//Require dependencies
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+  //Configurations loadin
+  //Ask for user imput
+  prompting: function(){
+    var done = this.async();
+    this.prompt({
+      type: 'input',
+      name: 'name',
+      message: 'Your app name',
+      default: this.appname
+    }, function(answers){
+      this.props = answers;
+      this.log(answers.name);
+      this.props.filename = answers.name+'.nsf';
+      done();
+    }.bind(this));
+  },
+  //Writing Logic
+  writing: {
+    //Copy the configuration files
+    config: function(){
+      this.fs.copyTpl(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json'), {
+          name: this.props.name
+        }
+      );
+      this.fs.copyTpl(
+        this.templatePath('_bower.json'),
+        this.destinationPath('bower.json'), {
+          name: this.props.name
+        }
+      );
+      this.fs.copy(
+        this.templatePath('_bowerrc'),
+        this.destinationPath('.bowerrc')
+      );
+    },
+    //Copy ODP's application files
+    app: function(){
+      //Main ODP
+      this.fs.copy(
+        this.templatePath('ODP'),
+        this.destinationPath('ODP')
+      );
+      //Templated files (to inject app name as received)
+      this.fs.copyTpl(
+        this.templatePath('_project'),
+        this.destinationPath('ODP/.project'), {
+          name: this.props.name+' ODP'
+        }
+      );
+      this.fs.copyTpl(
+        this.templatePath('_database.properties'),
+        this.destinationPath('ODP/AppProperties/database.properties'), {
+          name: this.props.name,
+          filename: this.props.filename
+        }
+      );
+      this.fs.copyTpl(
+        this.templatePath('_plugin.xml'),
+        this.destinationPath('ODP/plugin.xml'), {
+          name: this.props.name+' ODP'
+        }
+      );
+      this.fs.copyTpl(
+        this.templatePath('_IconNote'),
+        this.destinationPath('ODP/Resources/IconNote'), {
+          name: this.props.name
+        }
+      );
+    },
+    //Install Dependencies
+    install: function() {
+      //Currently no dependencies to install, keeping for consistency
+      this.installDependencies();
+    }
+  }
+});
