b01c8b2285a765ce11ce02935e91fb75c371062d
Eric McCormick <edm00se@users.noreply.github.com>
2016-03-31 11:30:11 -0500

    modified common script
    Wrapped update-notifier in CI check, for now. Probably best to make this fail more gracefully for older node versions; possible issue with yeoman/update-notifier?

--- common/index.js
+++ common/index.js
@@ -2,10 +2,12 @@ var updateNotifier = require('update-notifier');
 var path = require('path');
 
 module.exports = function () {
-  var nwPath = path.join(__dirname, '../package.json');
-  var pkg = require(nwPath);
-  if (pkg.version === null) {
-    pkg.version = "0.2.0";
+  if(!process.env.CI){
+    var nwPath = path.join(__dirname, '../package.json');
+    var pkg = require(nwPath);
+    if (pkg.version === null) {
+      pkg.version = "0.2.0";
+    }
+    updateNotifier({pkg}).notify();
   }
-  updateNotifier({pkg}).notify();
 };
