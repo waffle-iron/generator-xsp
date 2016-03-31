c19460b5bb6fa589be1d04d57cbdcd70bde8d265
Eric McCormick <edm00se@gmail.com>
2016-03-30 20:14:41 -0500

    updated tests for each
    refs #1

--- test/cc.js
+++ test/cc.js
@@ -2,16 +2,16 @@
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-generator').test;
-var testFileName = 'foo';
+var testFileName = 'bar';
 
-describe('generator-xsp:xpage', function () {
+describe('generator-xsp:cc', function () {
   before(function (done) {
-    helpers.run(path.join(__dirname, '../generators/xpage'))
+    helpers.run(path.join(__dirname, '../generators/cc'))
       .withArguments(testFileName)
       .on('end', done);
   });
 
   it('creates specified ODP XPage file', function () {
-    assert.file('ODP/XPages/' + testFileName + '.xsp');
+    assert.file('ODP/CustomControls/' + testFileName + '.xsp');
   });
 });
